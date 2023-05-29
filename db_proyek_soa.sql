c6583f5e2fc37af8f4312717f1af9dd2a91c1108
AlbertGunawan890 <117983382+AlbertGunawan890@users.noreply.github.com>
2023-05-29 18:48:08 +0700

    db

---
+++ db_proyek_soa.sql
@@ -0,0 +1,41 @@
+/*
+SQLyog Community v13.1.9 (64 bit)
+MySQL - 10.4.27-MariaDB : Database - db_proyek_soa
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_proyek_soa` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `db_proyek_soa`;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id_user` varchar(10) NOT NULL,
+  `nama_user` varchar(255) DEFAULT NULL,
+  `username_user` varchar(255) DEFAULT NULL,
+  `password_user` varchar(255) DEFAULT NULL,
+  `nik_user` varchar(16) DEFAULT NULL,
+  `alamat_user` varchar(255) DEFAULT NULL,
+  `notelp_user` varchar(15) DEFAULT NULL,
+  `tipe_user` varchar(30) DEFAULT NULL,
+  `saldo_user` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `users` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
